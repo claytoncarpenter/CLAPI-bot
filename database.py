d992ec994b20c78a762678d0bc0d63547842aab4
ozite <claytonwc672@gmail.com>
2021-03-05 13:18:39 -0600

    databse updates

--- database.py
+++ database.py
@@ -1,26 +1,24 @@
 import sqlite3
 import pandas as pd
 
-con = sqlite3.connect(r'database\discord.db')
-cur = con.cursor()
-#cur.execute("""DROP TABLE messages""")
-cur.execute("""CREATE TABLE IF NOT EXISTS messages (message_id PRIMARY KEY, author, author_id, content, guild, channel, created_at)""")
-
-
-
 def all_messages():
         df = pd.read_sql_query("SELECT * FROM messages", con)
         print(df)
 
-
-all_messages()
-
-        
 def insert(id, author, author_id, content, guild, channel, created_at):
         with sqlite3.connect(r'database\discord.db') as con:
                 cur = con.cursor()
                 cur.execute("INSERT INTO messages (message_id, author, author_id, content, guild, channel, created_at) VALUES (?,?,?,?,?,?,?)",(id, author, author_id, content, guild, channel, created_at))
                 con.commit()
+
+if __name__ == '__main__':
+        con = sqlite3.connect(r'database\discord.db')
+        cur = con.cursor()
+        cur.execute("""CREATE TABLE IF NOT EXISTS messages (message_id PRIMARY KEY, author, author_id, content, guild, channel, created_at)""")
+        #cur.execute("""DROP TABLE messages""")
+        all_messages()
+
+        
 
 
 
