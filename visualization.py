5de617f71347ace7d26a47c20f12c44044c6a03e
ozite <claytonwc672@gmail.com>
2021-03-08 08:36:38 -0600

    added exception handling to viz file

--- visualization.py
+++ visualization.py
@@ -3,33 +3,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def plot():
-    classics = 'The Classics'
-    data = database.query("SELECT * FROM messages") 
-    #WHERE guild = (?)", {classics})
-    print(data)
-    print(data.guild.unique())
+    try:
+        classics = 'The Classics'
+        data = database.query("SELECT * FROM messages") 
+        #WHERE guild = (?)", {classics})
+        print(data)
+        print(data.guild.unique())
+    except:
+        print('Error calling query from database')
+    else:
+        # plt.style.use('fivethirtyeight')
 
-    # plt.style.use('fivethirtyeight')
+        # x_values = []
+        # y_values = []
 
-    # x_values = []
-    # y_values = []
-
-    # index = count()
+        # index = count()
 
 
-    fig = plt.figure()
-    fig.set_size_inches(15,15)
-    grouped_data = data.groupby(by='author').describe()
-    print(grouped_data)
-    x_values = grouped_data.index
-    y_values = grouped_data['message_id']['count']
-    plt.bar(x_values, y_values)
-    plt.xlabel('Author')
-    plt.ylabel('Number of Comments')
-    plt.title('Discord Posts')
-    plt.xticks(rotation = 45)
-    plt.show()
-    # fig.savefig(r'static\plot.png')
-    # pass
-
-plot()
+        fig = plt.figure()
+        fig.set_size_inches(15,15)
+        grouped_data = data.groupby(by='author').describe()
+        print(grouped_data)
+        x_values = grouped_data.index
+        y_values = grouped_data['message_id']['count']
+        plt.bar(x_values, y_values)
+        plt.xlabel('Author')
+        plt.ylabel('Number of Comments')
+        plt.title('Discord Posts')
+        plt.xticks(rotation = 45)
+        plt.show()
+        # fig.savefig(r'static\plot.png')
+        # pass
+        plot()
