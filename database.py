54a52fbf95b1ff853ad94322eb25cb6842a64cd6
ozite <claytonwc672@gmail.com>
2021-03-07 20:51:29 -0600

    added functionality to filter bots

--- database.py
+++ database.py
@@ -3,24 +3,25 @@ import pandas as pd
 
 pd.options.display.max_colwidth = 100
 
-def query(sql):
+def query(sql, params=None):
         with sqlite3.connect(r'database\discord.db') as con:
-                df = pd.read_sql_query(sql , con)
+                df = pd.read_sql_query(sql , con, params=params)
                 print(df)
                 return df
 
-def insert(id, author, author_id, content, guild, channel, created_at):
+def insert(id, author, author_id, bot, content, guild, channel, created_at):
         with sqlite3.connect(r'database\discord.db') as con:
                 cur = con.cursor()
-                cur.execute("INSERT INTO messages (message_id, author, author_id, content, guild, channel, created_at) VALUES (?,?,?,?,?,?,?)",(id, author, author_id, content, guild, channel, created_at))
+                cur.execute("INSERT INTO messages (message_id, author, author_id, bot, content, guild, channel, created_at) VALUES (?,?,?,?,?,?,?)",(id, author, author_id, bot, content, guild, channel, created_at))
                 con.commit()
 
 if __name__ == '__main__':
         con = sqlite3.connect(r'database\discord.db')
         cur = con.cursor()
-        cur.execute("""CREATE TABLE IF NOT EXISTS messages (message_id PRIMARY KEY, author, author_id, content, guild, channel, created_at)""")
-        #cur.execute("""DROP TABLE messages""")
-        query("SELECT * FROM messages")
+        cur.execute("""DROP TABLE messages""")
+        cur.execute("""CREATE TABLE IF NOT EXISTS messages (message_id PRIMARY KEY, author, author_id, bot, content, guild, channel, created_at)""")
+        
+        query("SELECT * FROM messages",)
 
         
 
--- visualization.py
+++ visualization.py
@@ -1,5 +1,34 @@
 import database
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = database.query("SELECT * FROM messages")
-print(df['content'])
+def plot():
+    classics = 'The Classics'
+    data = database.query("SELECT * FROM messages WHERE guild = (?)", {classics})
+    #print(data)
+    print(data.guild.unique())
+
+    # plt.style.use('fivethirtyeight')
+
+    # x_values = []
+    # y_values = []
+
+    # index = count()
+
+
+    fig = plt.figure()
+    fig.set_size_inches(15,15)
+    grouped_data = data.groupby(by='author').describe()
+    #print(grouped_data)
+    x_values = grouped_data.index
+    y_values = grouped_data['message_id']['count']
+    plt.bar(x_values, y_values)
+    plt.xlabel('Author')
+    plt.ylabel('Number of Comments')
+    plt.title('Discord Posts')
+    plt.xticks(rotation = 45)
+    #plt.show()
+    # fig.savefig(r'static\plot.png')
+    # pass
+
+plot()
