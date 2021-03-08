a56e113e7896fbea4457e6bc49b9fcdc10d1080d
ozite <claytonwc672@gmail.com>
2021-03-08 08:20:41 -0600

    fixed bug with bot filter

--- database.py
+++ database.py
@@ -7,21 +7,22 @@ def query(sql, params=None):
         with sqlite3.connect(r'database\discord.db') as con:
                 df = pd.read_sql_query(sql , con, params=params)
                 print(df)
+                print('Test')
                 return df
 
 def insert(id, author, author_id, bot, content, guild, channel, created_at):
         with sqlite3.connect(r'database\discord.db') as con:
                 cur = con.cursor()
-                cur.execute("INSERT INTO messages (message_id, author, author_id, bot, content, guild, channel, created_at) VALUES (?,?,?,?,?,?,?)",(id, author, author_id, bot, content, guild, channel, created_at))
+                cur.execute("INSERT INTO messages (message_id, author, author_id, bot, content, guild, channel, created_at) VALUES (?,?,?,?,?,?,?,?)",(id, author, author_id, bot, content, guild, channel, created_at))
                 con.commit()
 
 if __name__ == '__main__':
         con = sqlite3.connect(r'database\discord.db')
         cur = con.cursor()
-        cur.execute("""DROP TABLE messages""")
+        #cur.execute("""DROP TABLE messages""")
         cur.execute("""CREATE TABLE IF NOT EXISTS messages (message_id PRIMARY KEY, author, author_id, bot, content, guild, channel, created_at)""")
         
-        query("SELECT * FROM messages",)
+        query("SELECT * FROM messages")
 
         
 
--- visualization.py
+++ visualization.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 
 def plot():
     classics = 'The Classics'
-    data = database.query("SELECT * FROM messages WHERE guild = (?)", {classics})
-    #print(data)
+    data = database.query("SELECT * FROM messages") 
+    #WHERE guild = (?)", {classics})
+    print(data)
     print(data.guild.unique())
 
     # plt.style.use('fivethirtyeight')
@@ -19,7 +20,7 @@ def plot():
     fig = plt.figure()
     fig.set_size_inches(15,15)
     grouped_data = data.groupby(by='author').describe()
-    #print(grouped_data)
+    print(grouped_data)
     x_values = grouped_data.index
     y_values = grouped_data['message_id']['count']
     plt.bar(x_values, y_values)
@@ -27,7 +28,7 @@ def plot():
     plt.ylabel('Number of Comments')
     plt.title('Discord Posts')
     plt.xticks(rotation = 45)
-    #plt.show()
+    plt.show()
     # fig.savefig(r'static\plot.png')
     # pass
 
